fe6234fd3e049d40ae80a1d80b9fd00f002099f4
tmackenzie01 <thomas.mackenzie@vmsuk.com>
2016-03-15 11:21:05 +0000

    Tidying up usings

--- DebugLogReader/Forms/frmDebugLogReader.cs
+++ DebugLogReader/Forms/frmDebugLogReader.cs
@@ -6,8 +6,6 @@ using System.Diagnostics;
 using System.Drawing;
 using System.Globalization;
 using System.IO;
-using System.Linq;
-using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
