8272cccc781ac326f66952a4d76bae82d6b069e9
tmackenzie01 <thomas.mackenzie@vmsuk.com>
2016-03-16 09:55:59 +0000

    Don't count the saving as part of the processing time

--- DebugLogReader/Forms/frmDebugLogReader.cs
+++ DebugLogReader/Forms/frmDebugLogReader.cs
@@ -568,6 +568,9 @@ namespace DebugLogReader
             String errorMessage = "";
             String giantLogFilename = Path.Combine(txtLogDirectory.Text, $"giantLog{m_filterDescription}.txt");
 
+            // Don't count the saving as part of the processing time
+            m_stpLogsProcessing.Stop();
+
             if (giantLog.Count == 0)
             {
                 saveFile = false;
@@ -649,7 +652,6 @@ namespace DebugLogReader
             {
                 AddMessage($"Failed to create log {errorMessage}");
             }
-            m_stpLogsProcessing.Stop();
             AddMessage($"Total time: {m_stpLogsProcessing.Elapsed.TotalSeconds.ToString("f3")} seconds");
 
             txtCombinedLog.Text = logFilename;
